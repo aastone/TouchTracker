cca6a4b06297c8b8739f1724d025c2a1bd0f3735
stone <stoneiswang@gmail.com>
2013-12-19 14:02:45 +0800

    Initial Commit

---
+++ TouchTracker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TouchTracker
+//
+//  Created by wangyipu on 13-12-19.
+//  Copyright (c) 2013年 stone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
